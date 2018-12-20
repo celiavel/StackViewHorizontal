ea7c7220e030806ba082d5ff64d962f081ba8241
celiavel <44905688+celiavel@users.noreply.github.com>
2018-12-20 20:43:06 +0100

    Add files via upload

---
+++ StackView Horizontal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView Horizontal
+//
+//  Created by Pacopb on 17/12/2018.
+//  Copyright © 2018 Celia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
